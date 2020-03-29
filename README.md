d9dbdaa4ebd3df56965a520a497e1eaf8b49760c
Rob <robert.j1450@gmail.com>
2020-03-28 20:00:32 -0400

    added initial files, setup node environment,  puppeteer, moment.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Web-scraper2
+
+* Web scraping air quality in New York using Node, puppeteer, moment.js
+* Headless browser
---
+++ app.js
@@ -0,0 +1,11 @@
+//import dependencies
+const puppeteer = require('puppeteer');
+const moment = require('moment');
+const fs = require('fs');
+
+(async () => {
+
+    //The location (url)
+    const url = 'https://aqicn.org/city/usa/newyork/';
+
+})();
